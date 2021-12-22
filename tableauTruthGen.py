c1057efe844e7b32806394dfac6337a40de383e1
patrik999 <patrik999@gmail.com>
2021-12-22 01:43:02 +0100

    New cases

--- tableauTruthGen.py
+++ tableauTruthGen.py
@@ -4,19 +4,36 @@ def getTruthValue(atom, truthValuesAtom):
 
     tValue = None
 
-    if '-' in atom:
-        tValue = truthValuesAtom[atom.replace('-', '')]
-        if(tValue <= 0):
-            tValue = 2
-        else:
-            tValue = tValue * (-1)
+    if '-~' in atom:
+        atomTemp = atom.replace('-', '').replace('~', '')
+        tValue = truthValuesAtom[atomTemp]
+        tValue = tValue * (-1)
+        tValue = calcWeakNeg(tValue)
+    elif '-' in atom:
+        countNeg = atom.count('-')
+        atomTemp = atom.replace('-', '').replace('~', '')
+        tValue = truthValuesAtom[atomTemp]
+
+        while countNeg >= 1:
+            tValue = calcWeakNeg(tValue)
+            countNeg=countNeg-1
+
     elif '~' in atom:
-        tValue = truthValuesAtom[atom.replace('~', '')]
+        atomTemp = atom.replace('-', '').replace('~', '')
+        tValue = truthValuesAtom[atomTemp]
         tValue = tValue * (-1)
     else:
         tValue = truthValuesAtom[atom]
 
     return tValue
+
+def calcWeakNeg(tValue):
+
+    if(tValue <= 0):
+        return 2
+    else:
+        return tValue * (-1)
+
 
 def binTruthValues(formula, truthValues):
 
@@ -58,56 +75,121 @@ def binTruthValues(formula, truthValues):
 
     return tValue
 
-#def main(argv):
+def test_tableau(allTruthValues):
 
-# Syntax: ; (or) | , (and) | - (wkneg) | ~ (wkneg) |  > (impl)
+    for tv1 in allTruthValues:
+        for tv2 in allTruthValues:
 
-# Iterate through all combinations
+            truthValues = {}
+            truthValues['a'] = tv1 # 0
+            truthValues['b'] = tv2 # 0
+            print("Input: " + str(truthValues))
 
-for tv1 in range(-2,3):
+            f1 = "a>b"
+            tVal1 = binTruthValues(f1, truthValues)
+            print(f1  +" has tv: " + str(tVal1))
 
-    for tv2 in range(-2,3):
+            f2 = "-a;b"
+            tVal2 = binTruthValues(f2, truthValues)
+            print(f2  +" has tv: " + str(tVal2))
 
-        truthValues = {}
-        truthValues['a'] = tv1 # 0
-        truthValues['b'] = tv2 # 0
+            f3 = "--a,-b"
+            tVal3 = binTruthValues(f2, truthValues)
+            print(f3  +" has tv: " + str(tVal3))
 
-        print("Input: " + str(truthValues))
-
-        f1 = "a;b"
-        tVal = binTruthValues(f1, truthValues)
-        #print(f1  +" has tv: " + str(tVal))
-
-        f2 = "a,b"
-        tVal = binTruthValues(f2, truthValues)
-        #print(f2  +" has tv: " + str(tVal))
-
-        f3 = "-a"
-        tVal = binTruthValues(f3, truthValues)
-        #print(f3  +" has tv: " + str(tVal))
-
-        f4 = "~a"
-        tVal = binTruthValues(f4, truthValues)
-        #print(f4  +" has tv: " + str(tVal))
-
-        f5 = "a>b"
-        tVal1 = binTruthValues(f5, truthValues)
-        #print(f5  +" has tv: " + str(tVal1))
-
-        f6 = "-a;b"
-        tVal2 = binTruthValues(f6, truthValues)
-        #print(f6  +" has tv: " + str(tVal2))
-
-        if tVal1 != tVal2:
-            print(f5  +" has tv: " + str(tVal1))
-            print(f6  +" has tv: " + str(tVal2))
-            print("DEVIATION")
-
-        #f7 = "~a;b"
-        #tVal = binTruthValues(f7, truthValues)
-        #print(f7  +" has tv: " + str(tVal))
+            if tVal1 != tVal2:
+                print("DEVIATION 1:")
+            if tVal1 != tVal3:
+                print("DEVIATION 2:")
 
 
-#if __name__ == "__main__":
-#    import sys
-#    main(sys.argv[1:])
+def test_doubleWeekNeg(allTruthValues):
+
+    for tv1 in allTruthValues:
+        for tv2 in allTruthValues:
+
+            truthValues = {}
+            truthValues['a'] = tv1 # 0
+            truthValues['b'] = tv2 # 0
+            print("Input: " + str(truthValues))
+
+            f1 = "a"
+            res1 = binTruthValues(f1, truthValues)
+            #print(f1  +" has tv: " + str(res1))
+
+            #f1a = "-a"
+            #tVal1a = binTruthValues(f1a, truthValues)
+            #print(f1a  +" has tv: " + str(tVal1a))
+
+            f2 = "--a"
+            res2 = binTruthValues(f2, truthValues)
+            print(f2  +" has tv: " + str(res2))
+
+            f3 = "-~a"
+            res3 = binTruthValues(f3, truthValues)
+            print(f3  +" has tv: " + str(res3))
+
+            if res1 != res2:
+                print("DEVIATION 1:")
+            if res1 != res3:
+                print("DEVIATION 2:")
+
+def test_impl(allTruthValues):
+
+    for tv1 in allTruthValues:
+        for tv2 in allTruthValues:
+
+            truthValues = {}
+            truthValues['a'] = tv1 # 0
+            truthValues['b'] = tv2 # 0
+
+            print("Input: " + str(truthValues))
+
+            f1 = "a;b"
+            tVal = binTruthValues(f1, truthValues)
+            #print(f1  +" has tv: " + str(tVal))
+
+            f2 = "a,b"
+            tVal = binTruthValues(f2, truthValues)
+            #print(f2  +" has tv: " + str(tVal))
+
+            f3 = "-a"
+            tVal = binTruthValues(f3, truthValues)
+            #print(f3  +" has tv: " + str(tVal))
+
+            f4 = "~a"
+            tVal = binTruthValues(f4, truthValues)
+            #print(f4  +" has tv: " + str(tVal))
+
+            f5 = "a>b"
+            tVal1 = binTruthValues(f5, truthValues)
+            #print(f5  +" has tv: " + str(tVal1))
+
+            f6 = "-a;b"
+            tVal2 = binTruthValues(f6, truthValues)
+            #print(f6  +" has tv: " + str(tVal2))
+
+            if tVal1 != tVal2:
+                print("DEVIATION:")
+                print(f5  +" has tv: " + str(tVal1))
+                print(f6  +" has tv: " + str(tVal2))
+
+            #f7 = "~a;b"
+            #tVal = binTruthValues(f7, truthValues)
+            #print(f7  +" has tv: " + str(tVal))
+
+
+if __name__ == "__main__":
+
+    # Syntax: ; (or) | , (and) | - (wkneg) | ~ (wkneg) |  > (impl)
+    # Iterate through all combinations
+
+    #allTruthValues = range(-2,3)
+    allTruthValues = {-2,0,2}
+    print(str(allTruthValues))
+
+    #test_impl(allTruthValues)
+    #test_doubleWeekNeg(allTruthValues)
+    test_tableau(allTruthValues)
+
+    #print("Everything passed")
